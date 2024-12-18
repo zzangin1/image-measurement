6757f386789cef4aa412cf0d5066783c10dfa767
zzangin <zzangin1919@gmail.com>
2024-12-18 20:40:37 +0900

    바인딩 변수 추가

---
+++ ImageMeasurement/ViewModel/UCImageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Media.Imaging;
+
+namespace ImageMeasurement.ViewModel
+{
+	public partial class UCImageViewModel : ObservableObject
+	{
+		#region => Field
+
+		[ObservableProperty]
+		private BitmapSource _displayImage;
+
+		#endregion => Field
+
+		#region => Property
+		#endregion => Property
+
+		#region => Constructor
+		#endregion => Constructor
+
+		#region => Method
+		#endregion => Method
+	}
+}
