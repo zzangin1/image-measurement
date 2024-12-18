32b96df72a2816754646d6a092336c363961cf5b
zzangin <zzangin1919@gmail.com>
2024-12-18 19:46:04 +0900

    초기 커밋.

---
+++ ImageMeasurement/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using ImageMeasurement.View;
+using ImageMeasurement.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace ImageMeasurement
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		#region => Property
+
+		public IServiceProvider Services { get; }
+		public new static App Current => (App)Application.Current;
+
+		#endregion => Property
+
+		#region => Constructor
+
+		public App()
+		{
+			Services = ConfigureServices();
+		}
+
+		#endregion => Constructor
+
+		#region => Method
+
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			var startView = App.Current.Services.GetService<WImageMeasurementView>();
+			startView.Show();
+		}
+
+		private static IServiceProvider ConfigureServices()
+		{
+			var services = new ServiceCollection();
+
+			// 뷰
+			services.AddSingleton<WImageMeasurementView>();
+
+			// 뷰모델
+			services.AddSingleton<WImageMeasurementViewModel>();
+
+			return services.BuildServiceProvider();
+		}
+
+		#endregion => Method
+	}
+
+}
---
+++ ImageMeasurement/View/WImageMeasurementView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using ImageMeasurement.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ImageMeasurement.View
+{
+	/// <summary>
+	/// WImageMeasurementView.xaml에 대한 상호 작용 논리
+	/// </summary>
+	public partial class WImageMeasurementView : Window
+	{
+		public WImageMeasurementView()
+		{
+			InitializeComponent();
+			DataContext = App.Current.Services.GetService<WImageMeasurementViewModel>();
+		}
+	}
+}
