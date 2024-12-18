ce588d89ce1f6d417ff4c36616b4c735406a065a
zzangin <zzangin1919@gmail.com>
2024-12-18 20:35:41 +0900

    코드 정리

--- ImageMeasurement/View/WImageMeasurementView.xaml.cs
+++ ImageMeasurement/View/WImageMeasurementView.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using ImageMeasurement.ViewModel;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace ImageMeasurement.View
 {
@@ -21,10 +9,14 @@ namespace ImageMeasurement.View
 	/// </summary>
 	public partial class WImageMeasurementView : Window
 	{
+		#region => Constructor
+
 		public WImageMeasurementView()
 		{
 			InitializeComponent();
 			DataContext = App.Current.Services.GetService<WImageMeasurementViewModel>();
 		}
+
+		#endregion => Constructor
 	}
 }
