387d1c918ac8bce1035653333dd2f61c33209db2
zzangin <zzangin1919@gmail.com>
2024-12-18 20:25:46 +0900

    이미지 파일 모델 구현

---
+++ ImageMeasurement/Model/ImageFileModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ImageMeasurement.Model
+{
+	public class ImageFileModel
+	{
+		#region => Property
+
+		public int Index { get; set; }
+		public string FilePath { get; set; }
+
+		#endregion => Property
+	}
+}
