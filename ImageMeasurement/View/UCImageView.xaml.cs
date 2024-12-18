d19cd42e598f5e5a8bc1f9dcef4e7e1c6b672a89
zzangin <zzangin1919@gmail.com>
2024-12-18 20:28:43 +0900

    뷰모델 주입

---
+++ ImageMeasurement/View/UCImageView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using ImageMeasurement.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows.Controls;
+
+namespace ImageMeasurement.View
+{
+	/// <summary>
+	/// UCImageView.xaml에 대한 상호 작용 논리
+	/// </summary>
+	public partial class UCImageView : UserControl
+	{
+		#region => Constructor
+
+		public UCImageView()
+		{
+			InitializeComponent();
+			DataContext = App.Current.Services.GetService<UCImageViewModel>();
+		}
+
+		#endregion => Constructor
+	}
+}
